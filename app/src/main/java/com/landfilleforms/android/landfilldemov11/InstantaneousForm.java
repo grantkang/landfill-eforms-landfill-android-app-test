95efef6b277062fe0f54f6df4187d412a22d79d1
Grant Kang <git.grantkang@gmail.com>
2017-01-19 12:09:01 -0800

    adding initial files

---
+++ app/src/main/java/com/landfilleforms/android/landfilldemov11/InstantaneousForm.java
@@ -0,0 +1,18 @@
+package com.landfilleforms.android.landfilldemov11;
+
+import java.util.Date;
+import java.util.UUID;
+
+/**
+ * Created by Work on 10/29/2016.
+ */
+
+public class InstantaneousForm {
+    private UUID mId;
+    private String mTitle;
+    private Date mStartDate;
+    private Date mEndDate;//This is going to be the same date as the start date but we need this to set the end time
+    private double ch4reading;
+
+
+}
